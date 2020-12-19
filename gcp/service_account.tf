b2d31f3e9d911a3e4e4a18daa5417130852d163b
J0hn-B <connect-wsman@hotmail.com>
2020-12-19 13:23:33 +0000

    update project id

--- gcp/service_account.tf
+++ gcp/service_account.tf
@@ -16,6 +16,17 @@ resource "google_project_iam_member" "config_connector" {
   member  = "serviceAccount:${google_service_account.config_connector.account_id}@${data.google_project.project.project_id}.iam.gserviceaccount.com"
 }
 
+resource "google_project_iam_member" "config_connector_storage" {
+  project = data.google_project.project.project_id
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.config_connector.account_id}@${data.google_project.project.project_id}.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "config_connector_editor" {
+  project = data.google_project.project.project_id
+  role    = "roles/editor"
+  member  = "serviceAccount:${google_service_account.config_connector.account_id}@${data.google_project.project.project_id}.iam.gserviceaccount.com"
+}
 
 resource "google_service_account_iam_member" "config_connector" {
   service_account_id = google_service_account.config_connector.name
